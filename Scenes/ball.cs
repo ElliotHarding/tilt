eb94b0d7b06f4b25fe623ab69e579a5c31bd177e
Elliot <elliot.one.harding@gmail.com>
2021-06-26 15:31:56 +0100

    wip - starting on code to fix ball clipping through platform

--- Scenes/ball.cs
+++ Scenes/ball.cs
@@ -14,10 +14,35 @@ public class ball : MonoBehaviour
     void Update()
     {
         
+    }   
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.name.Contains("Platform"))
+        {
+            GameObject platform = col.gameObject;
+
+            //find how close we are
+            float yDistBetweenBallAndPlatform = transform.position.y - platform.transform.position.y;
+            float requiredYDistance = platform.transform.lossyScale.y / 2 + transform.lossyScale.y / 2;
+
+            float correctionDifference = requiredYDistance - yDistBetweenBallAndPlatform;
+
+            if(correctionDifference > 0) //todo remove
+            {
+                Debug.Log("-----");
+                Debug.Log(yDistBetweenBallAndPlatform);
+                Debug.Log(requiredYDistance);
+
+                Debug.Log(correctionDifference);
+
+                transform.position = transform.position + new Vector3(0, correctionDifference);
+            }
+        }
     }
 
-    void OnCollisionEnter2D(Collision2D col)
+    void OnTriggerExit2D(Collider2D other)
     {
-        Debug.Log(col);
+        characterInQuicksand = false;
     }
 }
