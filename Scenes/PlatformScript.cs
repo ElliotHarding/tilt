11212db8b92d6a72534e68e6f2abb53957d896f3
Elliot <elliot.one.harding@gmail.com>
2021-06-27 14:08:46 +0100

    Walls swapping feature basic implementation

--- Scenes/PlatformScript.cs
+++ Scenes/PlatformScript.cs
@@ -13,10 +13,32 @@ public class PlatformScript : MonoBehaviour
     public GameObject m_leftWall;
     public float m_rightWallPos = 7;
     public float m_leftWallPos = -7;
-    public const int m_cSpawnBufferHeight = 10;
-    private float m_wallSpawnHeight = -10;
-    private float m_bottomWallSpawnHeight = 10;
-    private List<GameObject> m_walls = new List<GameObject>();
+    public float m_wallHeight = 10;
+    private const int m_cWallsSize = 3;
+    private int m_leftWallIndex = 0;
+    private int m_rightWallIndex = m_cWallsSize - 1;
+    private List<GameObject> m_leftWalls = new List<GameObject>();
+    private List<GameObject> m_rightWalls = new List<GameObject>();
+
+    void Start()
+    {
+        GameObject newRightWall = Instantiate(m_rightWall, new Vector3(m_rightWallPos, -m_wallHeight, 0), m_rightWall.transform.rotation);
+        GameObject newLeftWall = Instantiate(m_leftWall, new Vector3(m_leftWallPos, -m_wallHeight, 0), m_leftWall.transform.rotation);
+
+        GameObject newRightWall1 = Instantiate(m_rightWall, new Vector3(m_rightWallPos, 0, 0), m_rightWall.transform.rotation);
+        GameObject newLeftWall1 = Instantiate(m_leftWall, new Vector3(m_leftWallPos, 0, 0), m_leftWall.transform.rotation);
+
+        GameObject newRightWall2 = Instantiate(m_rightWall, new Vector3(m_rightWallPos, m_wallHeight, 0), m_rightWall.transform.rotation);
+        GameObject newLeftWall2 = Instantiate(m_leftWall, new Vector3(m_leftWallPos, m_wallHeight, 0), m_leftWall.transform.rotation);
+
+        m_rightWalls.Add(newRightWall);
+        m_leftWalls.Add(newLeftWall);
+        m_rightWalls.Add(newRightWall1);
+        m_leftWalls.Add(newLeftWall1);
+        m_rightWalls.Add(newRightWall2);
+        m_leftWalls.Add(newLeftWall2);
+
+    }
 
     // Update is called once per frame
     void FixedUpdate()
@@ -62,48 +84,28 @@ public class PlatformScript : MonoBehaviour
             setPlatformPositionAndRotation();
         }
 
-        //Check if need to spawn new set of walls
-        if(transform.position.y + m_cSpawnBufferHeight > m_wallSpawnHeight)
+        if(Input.GetKeyUp(KeyCode.Space))
         {
-            GameObject newRightWall = Instantiate(m_rightWall, new Vector3(m_rightWallPos, m_wallSpawnHeight, 0), m_rightWall.transform.rotation);
-            GameObject newLeftWall = Instantiate(m_leftWall, new Vector3(m_leftWallPos, m_wallSpawnHeight, 0), m_leftWall.transform.rotation);
-
-            m_walls.Add(newRightWall);
-            m_walls.Add(newLeftWall);
-
-            m_wallSpawnHeight += m_cSpawnBufferHeight;
+            Debug.Log("space pressed");
+            swapWallsUp();
         }
+    }
 
-        if(transform.position.y - m_cSpawnBufferHeight < m_bottomWallSpawnHeight)
-        {
-            GameObject newRightWall = Instantiate(m_rightWall, new Vector3(m_rightWallPos, m_bottomWallSpawnHeight, 0), m_rightWall.transform.rotation);
-            GameObject newLeftWall = Instantiate(m_leftWall, new Vector3(m_leftWallPos, m_bottomWallSpawnHeight, 0), m_leftWall.transform.rotation);
+    void swapWallsDown()
+    {
 
-            m_walls.Add(newRightWall);
-            m_walls.Add(newLeftWall);
+    }
 
-            m_bottomWallSpawnHeight -= m_cSpawnBufferHeight;
-        }
+    void swapWallsUp()
+    {
+        m_rightWalls[m_leftWallIndex].transform.position = m_rightWalls[m_rightWallIndex].transform.position + new Vector3(0, m_wallHeight);
+        m_leftWalls[m_leftWallIndex].transform.position = m_leftWalls[m_rightWallIndex].transform.position + new Vector3(0, m_wallHeight);
 
-        //Check to delete old walls
-        for(int i = 0; i < m_walls.Count; i++)
-        {
-            if (m_walls[i].transform.position.y < transform.position.y - m_cSpawnBufferHeight)
-            {
-                Destroy(m_walls[i]);
-                m_walls.RemoveAt(i);
-                m_bottomWallSpawnHeight += m_cSpawnBufferHeight / 2;
-                continue;
-            }
+        m_rightWallIndex = m_leftWallIndex;
+        m_leftWallIndex++;
 
-            if(m_walls[i].transform.position.y > transform.position.y + m_cSpawnBufferHeight)
-            {
-                Destroy(m_walls[i]);
-                m_walls.RemoveAt(i);
-                m_wallSpawnHeight -= m_cSpawnBufferHeight / 2;
-                continue;
-            }
-        }
+        if(m_leftWallIndex == m_cWallsSize)
+            m_leftWallIndex = 0;
     }
 
     void setPlatformPositionAndRotation()
