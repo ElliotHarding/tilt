af5c00a49149082209b8143f0390a66cfb0032b0
Elliot <elliot.one.harding@gmail.com>
2021-07-02 11:45:52 +0100

    Preventing ui clicks from triggering movers to move

--- Scenes/PlatformScript.cs
+++ Scenes/PlatformScript.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class PlatformScript : MonoBehaviour
 {
@@ -9,6 +11,10 @@ public class PlatformScript : MonoBehaviour
     public GameObject m_leftMover;
     public GameObject m_rightMover;
     public float m_moverSpeed = 10;
+
+    //Checking if mouse is clicking on UI
+    public EventSystem m_eventSystem;
+    public GraphicRaycaster m_raycaster;
 
     //wall spawning stuff
     public GameObject m_rightWall;
@@ -59,18 +65,22 @@ public class PlatformScript : MonoBehaviour
         {
             foreach (Touch touch in Input.touches)
             {
-                Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+                //Make sure isnt clicking on UI
+                if (!mouseOnUI(new Vector3(touch.position.x, touch.position.y)))
+                {
+                    Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
 
-                if (touchPos.x > 0)
-                {
-                    float yDiff = touchPos.y - m_rightMover.transform.position.y;
-                    m_rightMover.transform.position += new Vector3(0, yDiff * Time.deltaTime * m_moverSpeed, 0);
-                }
-                else
-                {
-                    float yDiff = touchPos.y - m_leftMover.transform.position.y;
-                    m_leftMover.transform.position += new Vector3(0, yDiff * Time.deltaTime * m_moverSpeed, 0);
-                }
+                    if (touchPos.x > 0)
+                    {
+                        float yDiff = touchPos.y - m_rightMover.transform.position.y;
+                        m_rightMover.transform.position += new Vector3(0, yDiff * Time.deltaTime * m_moverSpeed, 0);
+                    }
+                    else
+                    {
+                        float yDiff = touchPos.y - m_leftMover.transform.position.y;
+                        m_leftMover.transform.position += new Vector3(0, yDiff * Time.deltaTime * m_moverSpeed, 0);
+                    }
+                }               
             }
 
             setPlatformPositionAndRotation();
@@ -79,20 +89,24 @@ public class PlatformScript : MonoBehaviour
         //Handle mouse clicks
         if (Input.GetMouseButton(0))
         {
-            Vector2 mousePos = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
-
-            if (mousePos.x > 0)
+            //Make sure mouse isnt clicking on UI
+            if (!mouseOnUI(Input.mousePosition))
             {
-                float yDiff = mousePos.y - m_rightMover.transform.position.y;
-                m_rightMover.transform.position += new Vector3(0, yDiff * Time.deltaTime * m_moverSpeed, 0);
-            }
-            else
-            {
-                float yDiff = mousePos.y - m_leftMover.transform.position.y;
-                m_leftMover.transform.position += new Vector3(0, yDiff * Time.deltaTime * m_moverSpeed, 0);
-            }
+                Vector2 mousePos = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
 
-            setPlatformPositionAndRotation();
+                if (mousePos.x > 0)
+                {
+                    float yDiff = mousePos.y - m_rightMover.transform.position.y;
+                    m_rightMover.transform.position += new Vector3(0, yDiff * Time.deltaTime * m_moverSpeed, 0);
+                }
+                else
+                {
+                    float yDiff = mousePos.y - m_leftMover.transform.position.y;
+                    m_leftMover.transform.position += new Vector3(0, yDiff * Time.deltaTime * m_moverSpeed, 0);
+                }
+
+                setPlatformPositionAndRotation();
+            }            
         }
 
         //Check if were moving up and organise walls appropriatly
@@ -142,6 +156,17 @@ public class PlatformScript : MonoBehaviour
                 }
             }
         }
+    }
+
+    bool mouseOnUI(Vector3 mousePosition)
+    {
+        //Check were not clicking in ui area
+        List<RaycastResult> results = new List<RaycastResult>();
+        PointerEventData pointerEventData = new PointerEventData(m_eventSystem);
+        pointerEventData.position = mousePosition;
+        m_raycaster.Raycast(pointerEventData, results);
+
+        return results.Count != 0;
     }
 
     void spawnEnemy(bool above)
